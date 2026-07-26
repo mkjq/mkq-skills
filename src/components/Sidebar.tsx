76dba0959b542d6682b26c62667794974a64b911
Mohammad Khaled Qatanany <hakareo11@gmail.com>
2026-07-26 07:54:29 +0300

    Update and refactor: src/components/Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -39,12 +39,7 @@ export default function Sidebar() {
 
         <Link href="/dashboard" style={{ textDecoration: 'none' }}>
           <span style={{ fontSize: '0.8rem', color: 'var(--text-muted)', fontWeight: '700', marginTop: '10px', marginBottom: '8px', paddingInlineStart: '8px', textTransform: 'uppercase', letterSpacing: '1px', display: 'flex', alignItems: 'center', gap: '6px', cursor: 'pointer' }}>
-            <LayoutDashboard size={14} /> المكتبة السحابية
-          </span>
-        </Link>
-        <Link href="/dashboard/library" style={{ textDecoration: 'none' }}>
-          <span style={{ fontSize: '0.8rem', color: 'var(--text-muted)', fontWeight: '700', marginTop: '10px', marginBottom: '8px', paddingInlineStart: '8px', textTransform: 'uppercase', letterSpacing: '1px', display: 'flex', alignItems: 'center', gap: '6px', cursor: 'pointer' }}>
-            <LayoutDashboard size={14} /> إدارة المكتبة
+            <LayoutDashboard size={14} /> المكتبة السحابية للمهارات
           </span>
         </Link>
         
